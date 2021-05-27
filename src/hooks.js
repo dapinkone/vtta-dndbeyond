4fb2cf55d2097354334d1142d7bea3fae593f6f8
root <peter.iriarte24@gmail.com>
2021-05-27 00:54:36 +0000

    commented some stuff

--- src/hooks.js
+++ src/hooks.js
@@ -7,23 +7,23 @@ import OutgoingCommunication from "./messaging/outgoing.js";
 import setupLogging from "./hooks/init/setupLogging.js";
 
 // ready hooks
-import registerSheets from "./hooks/ready/registerSheets.js";
-import checkCompendiums from "./hooks/ready/checkCompendiums.js";
+//import registerSheets from "./hooks/ready/registerSheets.js";
+//import checkCompendiums from "./hooks/ready/checkCompendiums.js";
 import repairGameSettings from "./hooks/ready/repairGameSettings.js";
 import registerGameSettings from "./hooks/ready/registerGameSettings.js";
-import extendSceneNavigationContext from "./hooks/getSceneNavigationContext/extendSceneNavigationContext.js";
+//import extendSceneNavigationContext from "./hooks/getSceneNavigationContext/extendSceneNavigationContext.js";
 
 // other hooks
-import addFolderLabel from "./hooks/renderSidebarTab/addFolderLabel.js";
+//import addFolderLabel from "./hooks/renderSidebarTab/addFolderLabel.js";
 import linkImages from "./hooks/renderJournalSheet/linkImages.js";
-import { tutorialSetup } from "./tutorial/index.js";
-import showPopup from "./popup.js";
-import checkVersion from "./hooks/init/checkVersion.js";
-import checkElectron from "./hooks/ready/checkElectron.js";
-import displayConnectionIndicator from "./hooks/renderPlayerList/displayConnectionIndicator.js";
+//import { tutorialSetup } from "./tutorial/index.js";
+//import showPopup from "./popup.js";
+//import checkVersion from "./hooks/init/checkVersion.js";
+//import checkElectron from "./hooks/ready/checkElectron.js";
+//import displayConnectionIndicator from "./hooks/renderPlayerList/displayConnectionIndicator.js
 
 // renderNoteConfig
-import addNumberedIcons from "./hooks/renderNoteConfig/addNumberedIcons.js";
+//import addNumberedIcons from "./hooks/renderNoteConfig/addNumberedIcons.js";
 
 // socket messaging
 import onSocketMessage from "./hooks/socket/onSocketMessage.js";
@@ -43,30 +43,30 @@ export function onceReady() {
   repairGameSettings();
 
   // check for valid compendiums
-  checkCompendiums();
+//  checkCompendiums();
 
   // check for the running version
-  checkVersion();
+//  checkVersion();
 
   // check if ran in electron app
-  checkElectron();
+//  checkElectron();
 
   // delay the startup just a tiny little bit
-  setTimeout(() => {
-    utils.log("Starting EventPort", "messaging");
-    let port = new EventPort();
-    port.start();
+  // setTimeout(() => {
+  //   utils.log("Starting EventPort", "messaging");
+  //   let port = new EventPort();
+  //   port.start();
 
-    let com = OutgoingCommunication(port);
+  //   let com = OutgoingCommunication(port);
 
-    // register the D&DBeyond Button on the character sheets
-    registerSheets();
+  //   // register the D&DBeyond Button on the character sheets
+  //   registerSheets();
 
-    // send a notification to dndbeyond that it should update the actor data
-    Hooks.on("preUpdateActor", com.updateActorHP);
+  //   // send a notification to dndbeyond that it should update the actor data
+  // //  Hooks.on("preUpdateActor", com.updateActorHP);
 
-    showPopup().then(() => tutorialSetup());
-  }, 500);
+  //   showPopup().then(() => tutorialSetup());
+  // }, 500);
 }
 
 export function onReady() {
@@ -83,24 +83,24 @@ export function onReady() {
 
 // these functions are hooked in, we don't use all the data, so lets stop eslint complaining
 /* eslint-disable no-unused-vars */
-export function renderSidebarTab(directory, html, user) {
-  addFolderLabel(html);
-}
+// export function renderSidebarTab(directory, html, user) {
+//   addFolderLabel(html);
+// }
 
 export function renderJournalSheet(sheet, html, data) {
   linkImages(html);
 }
 
-export function getSceneNavigationContext(html, contextItems) {
-  extendSceneNavigationContext(html, contextItems);
-}
+// export function getSceneNavigationContext(html, contextItems) {
+//   extendSceneNavigationContext(html, contextItems);
+// }
 
-export function renderPLayerList(app, html) {
-  displayConnectionIndicator(app, html);
-}
+// export function renderPLayerList(app, html) {
+//   displayConnectionIndicator(app, html);
+// }
 
-export function renderNoteConfig(app, html, data) {
-  addNumberedIcons(app, html, data);
-}
+// export function renderNoteConfig(app, html, data) {
+//   addNumberedIcons(app, html, data);
+// }
 
 /* eslint-enable no-unused-vars */
--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import {
   init,
   onReady,
   onceReady,
-  renderSidebarTab,
+//  renderSidebarTab,
   renderJournalSheet,
-  renderPLayerList,
-  renderNoteConfig,
+//  renderPLayerList,
+//  renderNoteConfig,
 } from "./hooks.js";
-import extendSceneNavigationContext from "./hooks/getSceneNavigationContext/extendSceneNavigationContext.js";
+//import extendSceneNavigationContext from "./hooks/getSceneNavigationContext/extendSceneNavigationContext.js";
 
 CONFIG.debug.hooks = false;
 // register hooks
 Hooks.once("init", init);
 Hooks.once("ready", onceReady);
 Hooks.on("ready", onReady);
-Hooks.on("renderSidebarTab", renderSidebarTab);
+//Hooks.on("renderSidebarTab", renderSidebarTab);
 Hooks.on("renderJournalSheet", renderJournalSheet);
-Hooks.on("getSceneNavigationContext", extendSceneNavigationContext);
-Hooks.on("renderPlayerList", renderPLayerList);
-Hooks.on("renderNoteConfig", renderNoteConfig);
+//Hooks.on("getSceneNavigationContext", extendSceneNavigationContext);
+//Hooks.on("renderPlayerList", renderPLayerList);
+//Hooks.on("renderNoteConfig", renderNoteConfig);
